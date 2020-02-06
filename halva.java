dca742a95195366008afaa3eb0e671af34f5beac
sirisha00 <55032030+sirisha00@users.noreply.github.com>
2020-02-06 23:42:57 +0530

    Create halva.java

---
+++ halva.java
@@ -0,0 +1,13 @@
+package com.epam.lab.model.sweets;
+
+public class Halva extends Sweets {
+
+
+    public Halva() {
+    }
+
+    public Halva(double aSugar, double aWeight) {
+        setSugarLevel(aSugar);
+        setWeight(aWeight);
+    }
+}
