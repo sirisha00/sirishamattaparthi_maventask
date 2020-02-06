fcfd3021a70b13781541cada040b3b62582bbcc7
sirisha00 <55032030+sirisha00@users.noreply.github.com>
2020-02-06 23:41:36 +0530

    Create darkchocolate.java

---
+++ darkchocolate.java
@@ -0,0 +1,13 @@
+package maven1.sweets;
+
+public class DarkChocolate extends Sweets {
+
+    public DarkChocolate() {
+    }
+
+    public DarkChocolate(double aSugarLevel, double aWeight) {
+        setSugarLevel(aSugarLevel);
+        setWeight(aWeight);
+    }
+
+}
