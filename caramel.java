e96a13fa84e935d7c86a653bd7746600d221a7b6
sirisha00 <55032030+sirisha00@users.noreply.github.com>
2020-02-06 23:40:24 +0530

    Create caramel.java

---
+++ caramel.java
@@ -0,0 +1,13 @@
+package maven1.sweets;
+
+public class Caramel extends Sweets {
+
+    public Caramel() {
+    }
+
+    public Caramel(double aSugarLevel, double aWeight) {
+        setSugarLevel(aSugarLevel);
+        setWeight(aWeight);
+    }
+
+}
